05a146cdd66ac533634a3b7b550d82e2d1f0f1c8
Songchuyue <2083681987@qq.com>
2025-01-16 13:16:32 +0800

    Frontend and backend interaction implementation

--- backend/src/main/java/com/kingofhex/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kingofhex/backend/controller/pk/BotInfoController.java
@@ -3,20 +3,17 @@ package com.kingofhex.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
 	@RequestMapping("getbotinfo/")
-	public List<String> getBotInfo() {
-		List<String> list = new LinkedList<>();
-		list.add("bot1");
-		list.add("bot2");
-		list.add("bot3");
-		return list;
+	public Map<String, String> getBotInfo() {
+		Map<String, String> botInfo001 = new HashMap<>();
+		botInfo001.put("botName", "Genshin Impact professional player");
+		botInfo001.put("botVersion", "1.0.0");
+		return botInfo001;
 	}
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <div>Bot name: {{ bot_name }}</div>
+    <div>Bot version: {{ bot_version }}</div>
+  </div>
   <router-view/>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default {
+  name: "App",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_version = ref("");
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getbotinfo/",
+      type: "get",
+      success: resp => {
+        bot_name.value = resp.botName;
+        bot_version.value = resp.botVersion;
+      }
+    });
+
+    return {
+      bot_name,
+      bot_version
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url("@/assets/KamisatoAyakaBackground.png");
+  background-size: cover;
 }
 </style>
